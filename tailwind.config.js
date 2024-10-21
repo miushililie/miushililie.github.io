1a85d5de25d12d1d2cfccaf94fc2f233cf2f1e19
lvciddreams <farrelmaahira104@gmail.com>
2024-10-22 00:33:34 +0700

    new commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,14 @@ module.exports = {
   theme: {
     extend: {
       keyframes: {
+        "slide-in-right" : {
+          '0%': { transform: 'translateX(100%)', opacity: 0 },
+          '100%': { transform: 'translateX(0)', opacity: 1 },
+        },
+        "slide-in-left" : {
+          '0%': { transform: 'translateX(-100%)', opacity: 0 },
+          '100%': { transform: 'translateX(0)', opacity: 1 },
+        },
         "fade-in-down": {
           "0%": {
             opacity: 0,
@@ -16,7 +24,9 @@ module.exports = {
         },
       },
       animation : {
-        fadeindown : 'fade-in-down 1s ease-in 0.25s 1'
+        fadeindown : 'fade-in-down 1s ease-in 0.25s 1',
+        'slide-in-right' : 'slide-in-right 5s ease-out forwards',
+        'slide-in-left' : 'slide-in-left 5s ease-out forwards'
       }
     },
   },
